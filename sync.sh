00c09c67076f79cd8217e53d3343171bc4341c56
brunonotes <brunonotes7@gmail.com>
2021-11-28 15:10:46 -0300

    added sync script

---
+++ sync.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+rsync -av "/home/bruno/Documents/Pessoal/joplin/" "/media/Arquivo/Pessoal/joplin/"
+rsync -av "/media/Arquivo/Pessoal/Senhas/" "/home/bruno/Documents/Pessoal/senhas/"
