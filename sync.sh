a8d8cd9dbae80f2a9f73b8d4fcb7e676a8074ba5
BrunoNotes <brunonotes7@gmail.com>
2022-02-12 20:37:46 -0300

    changed sync directories

--- sync.sh
+++ sync.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-rsync -av "/home/bruno/Documents/joplin/" "/mnt/arquivo/Pessoal/joplin/"
-rsync -av "/home/bruno/Documents/senhas/" "/mnt/arquivo/Pessoal/Senhas/"
 rsync -av "/mnt/arquivo/Pessoal/" "/mnt/arquivo2/backup/pessoal/"
 rsync -av "/mnt/arquivo/Faculdade/" "/mnt/arquivo2/backup/faculdade/"
 rsync -av "/mnt/arquivo/Alura/" "/mnt/arquivo2/backup/alura/"
 rsync -av "/mnt/arquivo/imagens/" "/mnt/arquivo2/backup/imagens/"
+rsync -av "/home/bruno/Documents/bruno/" "/mnt/arquivo/Pessoal/bruno"
