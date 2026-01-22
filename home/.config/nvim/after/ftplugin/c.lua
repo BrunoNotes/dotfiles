d0a839b06f6f8fd56c4d56f5025338069fda029f
Bruno <brunonotes7@gmail.com>
2026-01-22 00:05:19 -0300

    update

--- home/.config/nvim/after/ftplugin/c.lua
+++ home/.config/nvim/after/ftplugin/c.lua
@@ -4,15 +4,17 @@ local path = vim.fn.getcwd() .. "/.clang-format"
 
 local text = [[
 ---
-Language:        C
+Language: C
 AllowShortFunctionsOnASingleLine: None
-ColumnLimit: 0
+ColumnLimit: 80
 IndentWidth: 4
 UseTab: Never
 PointerAlignment: Left
 SortIncludes: false
 BreakBeforeBraces: Attach
-AlignAfterOpenBracket: false
+AlignAfterOpenBracket: BlockIndent
+BinPackParameters: false
+AllowAllParametersOfDeclarationOnNextLine: true
 ]]
 
 vim.api.nvim_create_user_command("GenClangFormat", function()
